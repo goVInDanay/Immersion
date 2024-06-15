6002a80f0a04c287f19ca7c146d6d61eb98159da
Govind N Acharya <govindnacharya@gmail.com>
2024-06-15 15:41:00 +0530

    Committed

---
+++ src/Assignment2/LeetCode_414_Third_Maaximum_Number.java
@@ -0,0 +1,37 @@
+package Assignment2;
+import java.util.*;
+class Solution_Third_Max{
+    public int thirdMax(int  []nums) {
+        if(nums.length==0)
+            return -1;
+        Integer max1 = null;
+        Integer max2 = null;
+        Integer max3 = null;
+        for (Integer n : nums) {
+            if (n.equals(max1) || n.equals(max2) || n.equals(max3)) continue;
+            if (max1 == null || n > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = n;
+            } else if (max2 == null || n > max2) {
+                max3 = max2;
+                max2 = n;
+            } else if (max3 == null || n > max3) {
+                max3 = n;
+            }
+        }
+        return max3 == null ? max1 : max3;
+    }
+}
+public class LeetCode_414_Third_Maaximum_Number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int []arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        Solution_Third_Max s = new Solution_Third_Max();
+        System.out.println(s.thirdMax(arr));
+    }
+}
